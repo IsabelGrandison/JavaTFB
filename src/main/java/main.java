129655ef8806ee79336db4afa805e901da3271dc
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-19 15:20:46 -0400

    x and y variables

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,11 @@
+import java.awt.*;
+
 public class main {
+
     public static void main(String[] args) {
-        System.out.println("Java tutorial for beginners.");
+        Point point1 = new Point (1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
